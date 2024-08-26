4e454fd0e7fe90e2bd2b70b03afb1de3738f8662
Vinicius Moraes Tavares <130687376+ViniciusMoraesTavares@users.noreply.github.com>
2024-08-26 16:22:32 -0300

    Add files via upload
    Primeiro envio

---
+++ Pagina pessoal/Site-Pessoal/site vini/script.js
@@ -0,0 +1,9 @@
+function mudarCor(cor){
+    document.body.style.backgroundImage = "none"; 
+    document.body.style.backgroundColor = cor; 
+}
+
+function resetarCor(){
+    document.body.style.backgroundImage = "url('Plano-de-fundo-de-programacao-em-ultra-HD-1024x576.png')"; 
+    document.body.style.backgroundColor = "transparent"; 
+}
